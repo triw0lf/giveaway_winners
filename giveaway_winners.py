15d2884649ac37bc9ca18689605695f52b9d630e
Lauren P <l.e.proehl@gmail.com>
2020-01-24 18:12:36 +0000

    Create giveaway_winners.py

---
+++ giveaway_winners.py
@@ -0,0 +1,29 @@
+import random
+import colorama
+import time
+
+entries = ['hevnsnt', 'Fackque99', 'helloimdijah', 'al_rasheed', 'ZavieraE', 'DsignDevCybrSec', 's3cr3th3r0', 'msredmond', '0xtyro', 'dcast777', 'some_amish_guy', 'NoelTautges', 'ironmonkus', 'pjarmstrongaz', 'rlerne', 'HelpdeskMiles', 'securitystu', 'crabapple99', 'lanche29', 'ItsPirrip', 'bolonobolo', 'HemanshuKapadia', 'wyoham', 'tao_fd', 'tpetrovski831', 'STEMLatina', 'DesertPunk_', 'Bashu77', 'escn', 'JenF3rr_', 'Best_Chogath_NA', '_ztackett', '4NDR35__', 'Tim_kerr66', 'geekymutt', 'Whimmery', 'godslittlemacro', 'hun_warlord', 'Logan_Majin']
+
+time.sleep(2)
+
+print('\n')
+print('\033[1m' + 'Thank you all for entering to win and sharing your projects! They were very inspiring!')
+print('\n')
+time.sleep(2)
+print('\033[0m' + 'And the winners are...')
+print('\n')
+time.sleep(2)
+print('\033[92;1m' + '@' + random.choice(entries))
+print('\n')
+time.sleep(2)
+print('\033[92;1m' + '@' + random.choice(entries))
+print('\n')
+time.sleep(2)
+print('\033[92;1m' + '@' + random.choice(entries))
+time.sleep(2)
+print('\n')
+
+print('\033[0m' + 'Winners will be contacted by DM!')
+print('\033[0m' + 'Be sure to attend BSides Kansas City 2020 for another opportunity to win.')
+
+print('\n')
